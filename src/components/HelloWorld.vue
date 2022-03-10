e9af7e774d495eea5b98c25508e71aedb34e84dc
chzzy <chzzytool@163.com>
2022-03-10 21:38:09 +0800

    change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,11 +10,24 @@ const count = ref(0);
 
 <template>
   <h1>{{ msg }}</h1>
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button type="button" class="btn" @click="count++">
+    Count is: {{ count }}
+  </button>
 </template>
 
 <style scoped>
 a {
   color: #42b983;
+}
+.btn {
+  /* display: inline-flex; */
+  /* display: block; */
+  font-size: 16px;
+  color: blue;
+  border: 1px solid #ccc;
+  background-color: #ccc;
+  border-radius: 4px;
+  padding: 5px 16px;
+  cursor: pointer;
 }
 </style>
