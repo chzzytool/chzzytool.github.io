e1b4f471eb67ede05730498ff98e7c55d19a61d3
chzzy <chinesezzy@163.com>
2022-07-04 14:41:21 +0800

    修改内容测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,12 +10,7 @@ const count = ref(0);
 
 <template>
   <h1>{{ msg }}</h1>
-
   <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //本地运行配置，以及反向代理配置
+  server: {
+    host: "localhost",
+    https: false, //是否启用 http 2
+    cors: true, //为开发服务器配置 CORS , 默认启用并允许任何源
+    open: true, //服务启动时自动在浏览器中打开应用
+    port: "9030",
+    // strictPort: false, //设为true时端口被占用则直接退出，不会尝试下一个可用端口
+  },
 });
